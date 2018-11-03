1e7d66805d9d8f7172da28ff5d278f6195d58974
Jose Stricklin <josestricklin@El-Diablo.local>
2018-11-03 00:54:39 -0600

    add updated get functions

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,69 @@
 const functions = require('firebase-functions');
-const firebase = require('firebase')
-require('firebase/database')
+const admin = require('firebase-admin');
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-exports.getUserMenus = functions.https.onRequest((req, res) => {
-    firebase.database().ref('restaurant/').on('value', function(snapshot){
-        res.send(snapshot.val())
-    })
 
+
+// exports.getUserMenus = functions.https.onRequest((req, res) => {
+//     let menuArr = []
+//     admin.database().ref('restaurants/').on('value', function(snapshot){
+//         snapshot.forEach((menu) => {
+//             let addMenu = {
+//                 id: menu.key,
+//                 menuData: menu
+//             }
+//             menuArr.push(addMenu);
+//             return false
+//         });
+//         // menuArr.push(snapshot.val())
+//         res.send({menus: menuArr})
+//     })
+
+// })
+exports.getUserMenus = functions.https.onRequest((req, res) => {
+    admin.database().ref('dummyRestaurants/').on('value', function(snapshot){
+        let menuArr = []
+        for(let restaurant in snapshot.val()){
+            Object.keys(snapshot.val()[restaurant]).map(addy => {
+                let menu = {
+                    name: restaurant,
+                    address: addy,
+                    menuData: snapshot.val()[restaurant][addy].menu
+                }
+                menuArr.push(menu)
+            })
+        }
+        res.send({menus: menuArr})
+    })
+})
+
+// exports.getMenu = functions.https.onRequest((req, res) => {
+//     let menuID = req.params[0].slice(1)
+//     admin.database().ref('restaurants/').orderByKey().equalTo(menuID).on('value', function(snapshot){
+//         res.send({menu: snapshot.val()})
+//     })
+// })
+exports.getMenu = functions.https.onRequest((req, res) => {
+    let params = req.params[0].slice(1).split('/')
+    let name = params[0]
+    let address = params[1]
+    admin.database().ref('dummyRestaurants/').orderByKey().equalTo(`${name}`).on('value', (snapshot)=>{
+        for( name in snapshot.val()  ){
+            for( menuAddress in snapshot.val()[name] ){
+                if (address == menuAddress){
+                    var newMenu = {
+                        name: name,
+                        address: address,
+                        menu: snapshot.val()[name][menuAddress].menu
+                    }
+                    res.send({menu: newMenu})
+                }
+            }
+        }
+    })
 })
